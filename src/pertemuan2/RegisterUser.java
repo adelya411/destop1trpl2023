74070a75ee6edead0ed178b118452918c060d2e1
adelya411 <adelyasuryani02@gmail.com>
2023-03-01 13:02:36 +0700

    JFrame Form

---
+++ src/pertemuan2/RegisterUser.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan2;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author hpG42
+ */
+public class RegisterUser {
+     
+    public static void main(String[] args) {
+        
+    //TODO code application logic here
+        JFrame j = new JFrame("JFrame Pertamamku");
+        
+        JLabel nama = new JLabel();
+        nama.setText("Nama");
+        nama.setBounds(50, 100, 100, 30);
+        j.add(nama);
+        
+        JLabel username = new JLabel();
+        username.setText("Username");
+        username.setBounds(50, 100, 100, 30);
+        j.add(username);
+        
+        JLabel password = new JLabel();
+        password.setText("Password");
+        password.setBounds(50, 150, 100, 30);
+        j.add(password);
+        
+        JLabel email = new JLabel();
+        email.setText("Email");
+        email.setBounds(50, 100, 100, 30);
+        j.add(email);
+        
+        JLabel nohp = new JLabel();
+        nohp.setText("No HP");
+        nohp.setBounds(50, 100, 100, 30);
+        j.add(nohp);
+        
+        JTextField namaTxt = new JTextField();
+        namaTxt.setBounds(150, 100, 200, 30);
+        j.add(namaTxt);
+        
+        JTextField usernameTxt = new JTextField();
+        usernameTxt.setBounds(150, 100, 200, 30);
+        j.add(usernameTxt);
+        
+        JPasswordField passwordTxt = new JPasswordField();
+        passwordTxt.setBounds(150, 150, 200, 30);
+        j.add(passwordTxt);
+        
+        JTextField emailTxt = new JTextField();
+        emailTxt.setBounds(150, 100, 200, 30);
+        j.add(emailTxt);
+        
+        JTextField nohpTxt = new JTextField();
+        nohpTxt.setBounds(150, 100, 200, 30);
+        j.add(nohpTxt);
+        
+        JButton registerButton = new JButton();
+        registerButton.setText("Login");
+        registerButton.setBounds(150, 200, 100, 50);
+        j.add(registerButton);
+        
+        JTextArea testTxt = new JTextArea();
+        testTxt.setBounds(100, 200, 200, 200);
+        j.add(testTxt);
+                
+        j.setSize(800, 800);
+        j.setLayout(null);
+        j.setVisible(true);
+    }  
+}
